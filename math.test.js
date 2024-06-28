566283531e7b81e8562a25e31454b6b4e8f9b018
ChatchawanKh <khamkhuna@gmail.com>
2024-06-28 13:13:17 +0700

    Create math.test.js

---
+++ math.test.js
@@ -0,0 +1,16 @@
+console {sum, multiply, divide} = require('./math');
+
+test('Adds 1+2 to equal 3', () => {
+    expect(sum(1,2)).toBe(3);
+});
+test('multiplies 2*3 to equal 6', () => {
+    expect(multiply(2,3)).toBe(6);
+});
+
+test('divide 6/2 to equal 3', () => {
+    expect(divide(6,2)).toBe(3);
+});
+
+// test('divide by zero', () => {
+//     expect(sum(1,2)).toBe(3);
+// });
